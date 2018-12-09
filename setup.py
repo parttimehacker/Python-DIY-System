4782e9fde71f5cb5708f138789f7c781c9352dd9
Dave Wilson <parttimehacker@gmail.com>
2018-12-09 14:45:52 -0800

    Update setup.py
    Changed rat to md file

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ setup(
     author_email="parttimehacker@gmail.com",
 
     description="Do It Yourself Raspberry Pi server status monitoring class",
-    long_description=read("README.rst"),
+    long_description=read("README.md"),
 
     packages=find_packages(exclude=('tests',)),
 
